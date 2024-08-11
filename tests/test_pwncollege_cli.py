ead3ab707d9483574952f32c9d429f4d5c44e805
Leonardo Taccari <iamleot@gmail.com>
2024-08-11 09:50:25 +0200

    Add initial tests
    Just structure/boilerplate.

---
+++ tests/test_pwncollege_cli.py
@@ -0,0 +1,12 @@
+import unittest
+
+import pwncollege_cli
+
+
+class TestPwnCollegeCLI(unittest.TestCase):
+    def test_nonce(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
